04172b70b70bcaeb8eb9f1cc9152bce123afa08b
Shterio <Shterio@users.noreply.github.com>
2019-11-03 17:36:15 +0100

    adding final files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Deck from './Deck/Deck';
+import JokeList from './JokeList/JokeList';
 
 function App() {
 	return (
 		<div className='App'>
 			<Deck />
+			<JokeList />
 		</div>
 	);
 }
